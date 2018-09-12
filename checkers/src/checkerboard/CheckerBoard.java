87f41e2a004fa2bd29e0d8161b773756ed599f43
Rafael Silva Santos <rafael.silva.sts@gmail.com>
2018-09-12 14:51:38 -0300

    Correção do método getHouseAt

--- checkers/src/checkerboard/CheckerBoard.java
+++ checkers/src/checkerboard/CheckerBoard.java
@@ -87,8 +87,8 @@ public class CheckerBoard extends JPanel {
 	
 	
 	public CheckerHouse getHouseAt(int row, int col) {
-		if(houses.containsKey(row * cols + col)) {
-			return houses.get(row * cols + col);
+		if(houses.containsKey(rows - row - 1)  * cols + col)) {
+			return houses.get(rows - row - 1)  * cols + col);
 		}
 		else {
 			return null;
